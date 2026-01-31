532fa947b6dd958485ca4c04f81284593419d593
KevinOverall <kmoverall@gmail.com>
2026-01-31 16:50:57 -0500

    Added a PartyGoer Spawning system

--- Assets/Scenes/Scripts/Room.cs
+++ Assets/Scenes/Scripts/Room.cs
@@ -1,10 +1,15 @@
 using UnityEngine;
 using System.Collections.Generic;
-using System;
 
 public class Room : MonoBehaviour
 {
     public Bounds Bounds;
+
+    public Vector3 RandomPointInside => new Vector3(
+        Random.Range(Bounds.min.x, Bounds.max.x),
+        Random.Range(Bounds.min.y, Bounds.max.y),
+        Random.Range(Bounds.min.z, Bounds.max.z)
+    );
 
     public bool IsInside(Vector3 point)
     {
